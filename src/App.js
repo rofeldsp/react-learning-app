e72bedb6a68910d9bda8a5d15be61712abc69504
igor.krasnov <igor.krasnov@corp.mail.ru>
2022-03-16 14:58:11 +0300

    15 lessons

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "https://google.com";
+
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    const [name, setName] = useState('mario');
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    }, [name]);
+
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
+        </div>
+    );
+}
+ 
+export default Home;
